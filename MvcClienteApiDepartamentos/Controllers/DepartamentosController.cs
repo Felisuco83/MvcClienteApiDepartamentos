eb18e0fbb93b616527fd4c3cb5b4c0100f162a5d
Felix Itiel Arino Gila <FelixItiel.Arino@tajamar365.com>
2021-03-04 10:54:23 +0100

    Finished

---
+++ MvcClienteApiDepartamentos/Controllers/DepartamentosController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcClienteApiDepartamentos.Models;
+using MvcClienteApiDepartamentos.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcClienteApiDepartamentos.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        ServiceDepartamentos ServiceApi;
+        public DepartamentosController(ServiceDepartamentos serviceapi)
+        {
+            this.ServiceApi = serviceapi;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ListDepartamentos()
+        {
+            return View(await this.ServiceApi.GetDepartamentosAsync());
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await this.ServiceApi.BuscarDepartamentoAsync(id));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View(await this.ServiceApi.BuscarDepartamentoAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Departamento dept)
+        {
+            await this.ServiceApi.ModificarDepartamentoAsync(dept.IdDepartamento, dept.Nombre, dept.Localidad);
+            return RedirectToAction("ListDepartamentos");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Departamento dept)
+        {
+            await this.ServiceApi.InsertarDepartamentoAsync(dept.IdDepartamento, dept.Nombre, dept.Localidad);
+            return RedirectToAction("ListDepartamentos");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await this.ServiceApi.DeleteDepartamentoAsync(id);
+            return RedirectToAction("ListDepartamentos");
+        }
+
+        public IActionResult ApiClienteAjax()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MvcClienteApiDepartamentos/Services/ServiceDepartamentos.cs
@@ -0,0 +1,104 @@
+﻿using MvcClienteApiDepartamentos.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcClienteApiDepartamentos.Services
+{
+    public class ServiceDepartamentos
+    {
+        Uri UriApi;
+        private MediaTypeWithQualityHeaderValue Header;
+
+        public ServiceDepartamentos(string url)
+        {
+            this.UriApi = new Uri(url);
+            this.Header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        private async Task<T> CallApi<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = this.UriApi;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response = await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+           string request = "api/departamentos";
+            return await this.CallApi<List<Departamento>>(request);
+        }
+
+        public async Task<Departamento> BuscarDepartamentoAsync(int id)
+        {
+           string request = "api/departamentos/" + id;
+            return await this.CallApi<Departamento>(request);
+        }
+
+        public async Task DeleteDepartamentoAsync(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/departamentos/" + id;
+                client.BaseAddress = this.UriApi;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response = await client.DeleteAsync(request);
+            }
+        }
+
+        public async Task InsertarDepartamentoAsync (int id, string nombre, string localidad)
+        {
+            Departamento dept = new Departamento();
+            dept.IdDepartamento = id;
+            dept.Nombre = nombre;
+            dept.Localidad = localidad;
+            string json = JsonConvert.SerializeObject(dept);
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/departamentos";
+                client.BaseAddress = this.UriApi;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                StringContent content = new StringContent(json,Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(request, content);
+            }
+        }
+
+        public async Task ModificarDepartamentoAsync(int id, string nombre, string localidad)
+        {
+            Departamento dept = new Departamento();
+            dept.IdDepartamento = id;
+            dept.Nombre = nombre;
+            dept.Localidad = localidad;
+            string json = JsonConvert.SerializeObject(dept);
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/departamentos";
+                client.BaseAddress = this.UriApi;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync(request, content);
+            }
+        }
+    }
+}
